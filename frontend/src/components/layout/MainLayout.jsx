b53640a3e8210db18cb522b17743727ecfe0bdcf
Sachin-8125 <devinewolf7@gmail.com>
2025-09-29 12:14:27 +0530

    MainLayout Component created

---
+++ frontend/src/components/layout/MainLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from './Sidebar';
+import Headbar from './Headbar';
+
+const MainLayout = () => {
+    return (
+        <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
+            <Sidebar />
+            <div className="flex-1 flex flex-col overflow-hidden">
+                <Headbar />
+                <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 dark:bg-gray-900 p-6">
+                    <div className="container mx-auto">
+                        <Outlet />
+                    </div>
+                </main>
+            </div>
+        </div>
+    );
+};
+
+export default MainLayout;
